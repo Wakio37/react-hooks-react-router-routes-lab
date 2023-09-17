53edbe59a7ddf70a99884e2439315d987e73cb7d
Peris Wakio <peris.kingori@student.moringaschool.com>
2023-09-17 12:44:07 +0000

    completed assignment

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,30 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <div>
+    <h1>Movies Page</h1>
+  <div>
+    {
+      movies.map(movie=>{
+        return <div>
+                <h2>{movie.title}</h2>
+                <p>{movie.time}</p>
+                <ul>
+                {movie.genres.map(genre=>{
+                  return <li>{genre}</li>
+                })}
+                </ul>
+              </div>
+
+      }
+        
+      )
+    }
+    </div>
+    </div>
+
+  );
 }
 
 export default Movies;
